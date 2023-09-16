8165f1beec77a19969927f1fc7ffa47f2d37bc5b
Andressa Rodrigues <andressa.rodrigues2172@gmail.com>
2023-09-16 19:56:08 -0300

    segunda alteração

---
+++ aula4.estruturascondicionais/array.1.js
@@ -0,0 +1,40 @@
+
+   
+   /*
+var jogador1 = 0;
+var jogador2 = 2;
+var placar;
+
+   jogador1 !== -1 && jogador2 !== -1 ? console.log('Os jogadores são válidos') :
+    console.log('Jogadores são válidos')
+    
+    
+    if(jogador1 > 0 && jogador2 == 0) {
+        console.log('Jogador 1 marcou ponto')
+        placar = jogador1 > jogador2;
+    }else if(jogador2 > 0 && jogador1 == 0) {
+        console.log('Jogador 2 marcou ponto')
+        placar = jogador2 > jogador1;
+    }
+    else{
+        console.log('Niguem marcou ponto')
+    }
+
+    switch (placar){
+        case placar = jogador1 > jogador2 :
+        console.log('Jogador 1 ganhou');
+        break;
+        case placar = jogador2 > jogador1:
+        console.log('Jogador 2 ganhou');
+        break;
+        default:
+        console.log('Ninguem ganhou');
+    } */
+    
+    let array = ['valor1', 'valor2', 'valor3', 'valor4',];
+    let object = { propriedade1: 'valor1', propriedade2: 'valor2', propriedade3: 'valor3', };
+    for (let indice = 0; indice < array.length; indice++) {
+        console.log(indice);
+    }
+
+    
